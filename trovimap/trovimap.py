b13891790d277b6f8c41c12b17b2ef56cd127962
David-Carrasco <david.carrasco.cunado@gmail.com>
2018-11-24 14:41:41 +0100

    Adding pagination get_properties_by_location function

--- trovimap/trovimap.py
+++ trovimap/trovimap.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import itertools
 #import glom
 
 
@@ -54,6 +55,24 @@ TYPE_ID_NEIGHBORHOOD = 25
 ##################
 
 
+# UTILS
+
+def request_page(url, payload_base, default_page_param='Page'):
+
+    # Request by page number
+    for page in itertools.count(1):
+
+        # Updating page parameter
+        payload_base.update({default_page_param: page})
+
+        # Request
+        r = requests.post(url=url, data=json.dumps(payload_base))
+
+        yield r.json()
+
+
+# MAIN API
+
 def get_search_location(location_name):
     '''
     :param location_name:
@@ -79,21 +98,24 @@ def get_properties_by_location(location):
     # Get location id for the param_name attribute
     location_id = get_location_by_text(location)[location.param_name]
 
-    # Request config
-    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'}
-
     # TODO - param_name is adapted until properties endpoint unifies the param_name for Neighborhood:
     #        (SublocalityId vs SubLocalityId)
     payload = ({'SubLocalityId': location_id}
                if isinstance(location, Neighborhood)
                else {location.param_name: location_id})
 
-    r = requests.post(MAP_SHAPE_URL, headers=headers, data=json.dumps(payload))
+    # Adding specific payload parameters for this endpoint
+    payload.update({'PageSize': 20, 'Precision': 5, 'SortField': '_VisualScore'})
 
-    return r.json()
+    # Getting properties ids in paginated requests until there is no properties
+    properties = []
+    for content in itertools.takewhile(lambda response: len(response['properties']),
+                                       request_page(url=MAP_SHAPE_URL, payload_base=payload)):
 
+        properties.append([property['Sysid'] for property in content['properties']])
 
+    # Flatting properties lists
+    properties = list(itertools.chain.from_iterable(properties))
 
-
-
+    return properties
 
